0e707415ae03711c654d82c7ba342681fad957cc
Fabio Fenoglio <fabio.fenoglio@gmail.com>
2020-04-14 15:39:24 +0200

    fixed readme on PYPI

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from distutils.core import setup
 
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
 	name = 'lelof1py',
 	packages = ['lelof1py'],
-	version = '0.3-alpha',
+	version = '0.4-alpha',
 	license = 'MIT',
 	description = 'Python client for LELO F1 SDK device',
-    long_description = 'file: README.md',
+    long_description = long_description,
     long_description_content_type = 'text/markdown',
 	author = 'Fabio Fenoglio',
 	author_email = 'development@fabiofenoglio.it',
 	url = 'https://github.com/fabiofenoglio/lelo-f1-python-sdk',
-	download_url = 'https://github.com/fabiofenoglio/lelo-f1-python-sdk/archive/v0.3-alpha.tar.gz',
+	download_url = 'https://github.com/fabiofenoglio/lelo-f1-python-sdk/archive/v0.4-alpha.tar.gz',
 	keywords = ['LELO', 'BLUETOOTH'],
 	install_requires = [
 		'asyncio',
